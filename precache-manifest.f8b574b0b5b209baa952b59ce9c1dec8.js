570f9bc44c875163babc10aebf24f9672f6d8289
Taras <senivtar1999@gmail.com>
2019-03-08 17:54:48 +0200

    Updates

---
+++ precache-manifest.f8b574b0b5b209baa952b59ce9c1dec8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0a39778a0bc5ccc7a63",
+    "url": "/data-table-react/build/static/css/main.2d3c888c.chunk.css"
+  },
+  {
+    "revision": "e0a39778a0bc5ccc7a63",
+    "url": "/data-table-react/build/static/js/main.292b084e.chunk.js"
+  },
+  {
+    "revision": "14bb187c717454fe3f71",
+    "url": "/data-table-react/build/static/js/runtime~main.05eb632c.js"
+  },
+  {
+    "revision": "d154ece1507e70d581d4",
+    "url": "/data-table-react/build/static/js/2.c4ecd5ad.chunk.js"
+  },
+  {
+    "revision": "99f6e68caf13f11d302c54e0cca5425f",
+    "url": "/data-table-react/build/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/data-table-react/precache-manifest.ec4a7c1eff734d77a812672612405491.js"
+  "/data-table-react/build/precache-manifest.f8b574b0b5b209baa952b59ce9c1dec8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/data-table-react/index.html", {
+workbox.routing.registerNavigationRoute("/data-table-react/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
