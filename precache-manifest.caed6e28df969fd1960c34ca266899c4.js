266a2f0ad48fa8d5551364c4c74d540f74dd63c1
Taras <senivtar1999@gmail.com>
2019-03-08 19:57:06 +0200

    Updates

---
+++ precache-manifest.caed6e28df969fd1960c34ca266899c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e688dfbd75dab40874ab",
+    "url": "/data-table-react/static/css/main.2d3c888c.chunk.css"
+  },
+  {
+    "revision": "e688dfbd75dab40874ab",
+    "url": "/data-table-react/static/js/main.e0589977.chunk.js"
+  },
+  {
+    "revision": "379ab15f92db041b85df",
+    "url": "/data-table-react/static/js/runtime~main.6629ad23.js"
+  },
+  {
+    "revision": "d154ece1507e70d581d4",
+    "url": "/data-table-react/static/js/2.c4ecd5ad.chunk.js"
+  },
+  {
+    "revision": "20197a47b0ad3ff08abe2ec68d583cbb",
+    "url": "/data-table-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/data-table-react/build/precache-manifest.1f4653c05770af56071cf0c97ebda7f7.js"
+  "/data-table-react/precache-manifest.caed6e28df969fd1960c34ca266899c4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/data-table-react/build/index.html", {
+workbox.routing.registerNavigationRoute("/data-table-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
