0b42d091a6ade40ffc66718f05390eb7430633d6
Taras <senivtar1999@gmail.com>
2019-03-08 19:55:42 +0200

    Updates

--- precache-manifest.1f4653c05770af56071cf0c97ebda7f7.js
+++ precache-manifest.1f4653c05770af56071cf0c97ebda7f7.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "e0a39778a0bc5ccc7a63",
+    "revision": "6415397c6561a3915a97",
     "url": "/data-table-react/build/static/css/main.2d3c888c.chunk.css"
   },
   {
-    "revision": "e0a39778a0bc5ccc7a63",
-    "url": "/data-table-react/build/static/js/main.292b084e.chunk.js"
+    "revision": "6415397c6561a3915a97",
+    "url": "/data-table-react/build/static/js/main.9ea496b6.chunk.js"
   },
   {
     "revision": "14bb187c717454fe3f71",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/data-table-react/build/static/js/2.c4ecd5ad.chunk.js"
   },
   {
-    "revision": "99f6e68caf13f11d302c54e0cca5425f",
+    "revision": "c0fb1388cd272b027c33c8c75a2f717a",
     "url": "/data-table-react/build/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/data-table-react/build/precache-manifest.f8b574b0b5b209baa952b59ce9c1dec8.js"
+  "/data-table-react/build/precache-manifest.1f4653c05770af56071cf0c97ebda7f7.js"
 );
 
 workbox.clientsClaim();
